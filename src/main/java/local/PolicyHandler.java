6e8d7fcfa3a095c88bc61ba812e68816c4c4bf0d
SungJae Kim <ksj0495@naver.com>
2021-07-26 14:18:34 +0900

    Update PolicyHandler.java

--- src/main/java/local/PolicyHandler.java
+++ src/main/java/local/PolicyHandler.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
+import java.util.Date;
 
 @Service
 public class PolicyHandler{
